a862202ea17fa4c699dc9cdd0181f4312a4f9eb8
saskia.baltin <saskia.baltin@khk.ee>
2021-02-23 15:29:14 +0200

    Style for front page buttons

--- src/views/FrontPage.jsx
+++ src/views/FrontPage.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import Background from "../components/Background";
+import { Link } from "react-router-dom";
 
 const FrontPage = () => {
   return (
-    <div className="position-relative">
-      <Background />
+    <div className="frontpage__btns position-relative d-flex flex-column align-items-center">
+      <Link
+        to="/signup"
+        className="mb-2 btn rounded-pill fw-bold 
+             bg-light text-primary border-primary"
+        aria-label="loo konto"
+      >
+        Loo konto
+      </Link>
+      <Link
+        to="/"
+        className="mb-2 btn rounded-pill fw-bold 
+          bg-transparent text-light border-light"
+        aria-label="logi sisse"
+      >
+        Logi sisse
+      </Link>
     </div>
   );
 };
