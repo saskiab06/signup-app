de2523c57f04c814399c0cfea10ccafe33057484
saskia.baltin <saskia.baltin@khk.ee>
2021-02-23 21:48:15 +0200

    Add bird icon to white button and hover effect to buttons

--- src/views/FrontPage.jsx
+++ src/views/FrontPage.jsx
@@ -4,22 +4,28 @@ import { Link } from "react-router-dom";
 const FrontPage = () => {
   return (
     <div className="frontpage__btns position-relative d-flex flex-column align-items-center">
-      <Link
-        to="/signup"
-        className="mb-2 btn rounded-pill fw-bold 
+      <div className="row flex-row-reverse justify-content-center w-75">
+        <Link
+          to="/signup"
+          className="col mb-3 ms-n1 btn rounded-pill fw-bold 
              bg-light text-primary border-primary"
-        aria-label="loo konto"
-      >
-        Loo konto
-      </Link>
-      <Link
-        to="/"
-        className="mb-2 btn rounded-pill fw-bold 
+          aria-label="loo konto"
+        >
+          Loo konto
+        </Link>
+        <span className="col-1 bird-icon position-relative"></span>
+      </div>
+      <div className="row flex-row-reverse justify-content-center w-75">
+        <Link
+          to="/"
+          className="col ms-n1 btn rounded-pill fw-bold 
           bg-transparent text-light border-light"
-        aria-label="logi sisse"
-      >
-        Logi sisse
-      </Link>
+          aria-label="logi sisse"
+        >
+          Logi sisse
+        </Link>
+        <span className="col-1"></span>
+      </div>
     </div>
   );
 };
