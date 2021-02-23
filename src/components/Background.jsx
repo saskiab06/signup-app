b5fbad4a0115c16361995db8342158a7ebb18503
saskia.baltin <saskia.baltin@khk.ee>
2021-02-23 20:28:46 +0200

    Add wave animation to SVG paths via <animate> tag

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -13,22 +13,102 @@ const Background = ({ clientWidth }) => {
     >
       <path
         transform="translate(0,18)"
-        d={`M0 40.3105 C92.0388 80.8932 175.079 -3.22818 276 0.0963014 C302.594 0.972354 330.43 6.49644 ${clientWidth} 19.8 V${svgHeight} H0 V40.3105 Z`}
+        d={`M0 40.3105 
+            C92.0388 80.8932 175.079 -3.22818 276 0.0963014 
+            C302.594 0.972354 330.43 6.49644 ${clientWidth} 19.8 
+            V${svgHeight} H0 V40.3105 Z`}
         fill="url(#gradient_linear)"
-      />
+      >
+        <animate
+          repeatCount="indefinite"
+          fill="url(#gradient_linear)"
+          attributeName="d"
+          dur="10s"
+          values={`
+            M0 40.3105 
+            C92.0388 80.8932 175.079 -3.22818 276 0.0963014 
+            C302.594 0.972354 330.43 6.49644 ${clientWidth} 19.8 
+            V${svgHeight} H0 V40.3105 Z;
+
+            M 0 40.3105 
+            C 100 -36 195 63 261 56 
+            C 300 56 334 44 ${clientWidth} 19.8 
+            V${svgHeight} H0 V40.3105 Z;
+
+            M0 40.3105 
+            C92.0388 80.8932 175.079 -3.22818 276 0.0963014 
+            C302.594 0.972354 330.43 6.49644 ${clientWidth} 19.8 
+            V${svgHeight} H0 V40.3105 Z
+            `}
+        ></animate>
+      </path>
 
       <path
         opacity="0.7"
         d={`M0 20.5 C33.0798 35.0848 108.997 51.8874 141 47.5 C221.504 36.4633 258.549 -29.1403 ${clientWidth} 16.5 V${svgHeight} H0 V20.5 Z`}
         fill="url(#gradient_linear)"
-      />
-
+      >
+        <animate
+          repeatCount="indefinite"
+          fill="url(#gradient_linear)"
+          attributeName="d"
+          dur="11s"
+          values={`
+        M 0 30.5
+        C 33.0798 35.0848 108.997 51.8874 141 47.5
+        C 221.504 36.4633 258.549 -29.1403 ${clientWidth} 30.5
+        V ${svgHeight} H 0 V 20.5 Z;
+       
+        M 0 30.5 C 33.0798 35.0848 56 15 110 14 C 199 15 271 85 ${clientWidth} 36.5 
+        V ${svgHeight} H 0 V 20.5 Z;  
+      
+        M 0 30.5
+        C 33.0798 35.084 108.997 51.8874 141 47.5
+        C 221.504 36.4633 258.549 -29.1403 ${clientWidth} 30.5
+        V ${svgHeight} H 0 V 20.5 Z;`}
+        ></animate>
+      </path>
       <path
         transform="translate(0,7)"
         opacity="0.5"
-        d={`M0 12.0864 C33.0798 27.1013 95.9971 9.51681 128 5 C208.504 -6.36212 258.549 5.01379 ${clientWidth} 42 V${svgHeight} H0 V12.0864 Z`}
+        d={`
+          M 0 25.1
+          C 33.08 27.10 96 9.52 128 5
+          C 208.5 -6.36 258.55 5 ${clientWidth} 42 
+          V${svgHeight} H0 V12.0864 Z
+          `}
         fill="url(#gradient_linear)"
-      />
+      >
+        <animate
+          repeatCount="indefinite"
+          fill="url(#gradient_linear)"
+          attributeName="d"
+          dur="9s"
+          values={`
+            M 0 25.1
+            C 33.08 27.10 96 9.52 128 5
+            C 208.5 -6.36 258.55 5 ${clientWidth} 42
+            V ${svgHeight}
+            H 0
+            V 12.0864
+            Z;
+
+            M 0 12.0864 C 33.0798 27.1013 79 37 130 43 
+            C 205 51 257 54 ${clientWidth} 42
+            V ${svgHeight}
+            H 0
+            V 12.0864
+            Z;
+          
+            M 0 25.1 
+            C 33.08 27.1 67 9.52 128 5 
+            C 208.5 -6.36 258.55 5 ${clientWidth} 42
+            V ${svgHeight}
+            H 0
+            V 12.0864
+            Z`}
+        ></animate>
+      </path>
 
       <defs>
         <linearGradient
