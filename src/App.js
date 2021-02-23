bb3022691f9ff754ca87e660ef8cf47dcb5e3985
saskia.baltin <saskia.baltin@khk.ee>
2021-02-23 12:37:15 +0200

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import FrontPage from "./views/FrontPage";
+import SignUp from "./views/SignUp";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/">
+          <FrontPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
