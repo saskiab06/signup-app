620e461c2f884e650172f8a836da329e1c6f5895
saskia.baltin <saskia.baltin@khk.ee>
2021-02-23 21:42:35 +0200

    Add resize effect hook to client dimensions calculator

--- src/utils/getClientDimensions.js
+++ src/utils/getClientDimensions.js
@@ -1,6 +1,7 @@
+import { useState, useEffect } from "react";
+
 /* This function gets the height and width of the html document 
  that are used to make the front page background somewhat responsive on mobile devices. */
-
 function getClientDimensions() {
   const el = document.querySelector("html");
   const height = el.clientHeight;
@@ -11,4 +12,22 @@ function getClientDimensions() {
   };
 }
 
-export default getClientDimensions;
+// Component for a useEffect hook is needed for Safari preview resize to work properly
+const useClientDimensions = () => {
+  const [clientDimensions, setClientDimensions] = useState(
+    getClientDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setClientDimensions(getClientDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return clientDimensions;
+};
+
+export default useClientDimensions;
